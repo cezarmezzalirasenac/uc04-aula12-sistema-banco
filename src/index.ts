188e8f86bdc8920ee579550e64a07020f5b7ec5f
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-12-05 21:56:07 -0300

    validação agencia e conta

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@
  * das operações feitas pela conta.
  */
 
+import { randomUUID } from "node:crypto";
 import Scanner from "@codeea/scanner";
 
 let scanner: Scanner;
@@ -24,6 +25,8 @@ type Conta = {
 type Transacao = {
   id: string;
   valor: number;
+  numeroConta: number;
+  agencia: number;
   tipo: TipoTransacao;
   operacao: TipoOperacao;
 };
@@ -32,13 +35,27 @@ type TipoTransacao = "E" | "S";
 
 type TipoOperacao = "SAQ" | "DEP" | "TRANSF" | "PIX";
 
+const contas: Conta[] = [];
+const transacoes: Transacao[] = [];
+
 async function main() {
+  const agencia = parseInt(
+    await scanner.question("Informe o número da agência: ")
+  );
+  const numeroConta = parseInt(
+    await scanner.question("Informe o número da conta: ")
+  );
+  let conta = localizaConta(agencia, numeroConta);
   let operacao = 0;
 
-  do {
+  if (!conta) {
+    console.log("Conta não encontrada!");
+  } else {
     imprimeMenu();
     operacao = parseInt(await scanner.question("Informe a operação: "));
+  }
 
+  while (true) {
     if (operacao === 0) {
       console.log("Obrigado por utilizar nossos serviços!\nVolte Sempre!");
       break;
@@ -49,8 +66,9 @@ async function main() {
     // 2 - DEPOSITO
     // 3 - SAQUE
     // 4 - EXTRATO
-
-  } while (true);
+    imprimeMenu();
+    operacao = parseInt(await scanner.question("Informe a operação: "));
+  }
 }
 
 function imprimeMenu() {
@@ -64,8 +82,36 @@ function imprimeMenu() {
   console.log(menu);
 }
 
+function inicializarBanco() {
+  const conta: Conta = {
+    nomeCliente: "Cezar Augusto Mezzalira",
+    numero: 1234,
+    agencia: 1,
+    saldo: 100,
+  };
+  contas.push(conta);
+  const transacao: Transacao = {
+    id: randomUUID(),
+    valor: 100,
+    numeroConta: conta.numero,
+    agencia: conta.agencia,
+    tipo: "E",
+    operacao: "DEP",
+  };
+  transacoes.push(transacao);
+}
+
+function localizaConta(agencia: number, numeroConta: number) {
+  for (let conta of contas) {
+    if (conta.agencia === agencia && conta.numero === numeroConta) {
+      return conta;
+    }
+  }
+}
+
 (async () => {
   scanner = new Scanner();
+  inicializarBanco();
   await main();
   scanner.close();
 })();
